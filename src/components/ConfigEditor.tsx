fc5b41ac6819cd070d28ad68d185387c8e328e3b
Volodymyr Kohut <vkohut@hydrolix.io>
2025-02-07 12:27:51 +0000

    HDX-7285 Simple configuration page for Grafana plugin

--- src/components/ConfigEditor.tsx
+++ src/components/ConfigEditor.tsx
@@ -1,5 +1,5 @@
 import React, { ChangeEvent } from 'react';
-import { InlineField, Input, SecretInput } from '@grafana/ui';
+import {InlineField, InlineSwitch, Input, Legend, SecretInput, Stack} from '@grafana/ui';
 import { DataSourcePluginOptionsEditorProps } from '@grafana/data';
 import { MyDataSourceOptions, MySecureJsonData } from '../types';
 
@@ -9,61 +9,126 @@ export function ConfigEditor(props: Props) {
   const { onOptionsChange, options } = props;
   const { jsonData, secureJsonFields, secureJsonData } = options;
 
-  const onPathChange = (event: ChangeEvent<HTMLInputElement>) => {
+  const onUserNameChange = (event: ChangeEvent<HTMLInputElement>) => {
     onOptionsChange({
       ...options,
       jsonData: {
         ...jsonData,
-        path: event.target.value,
+        username: event.target.value,
+      },
+    });
+  };
+  const onHostChange = (event: ChangeEvent<HTMLInputElement>) => {
+    onOptionsChange({
+      ...options,
+      jsonData: {
+        ...jsonData,
+        hostname: event.target.value,
+      },
+    });
+  };
+  const onPortChange = (event: ChangeEvent<HTMLInputElement>) => {
+    onOptionsChange({
+      ...options,
+      jsonData: {
+        ...jsonData,
+        port: +event.target.value,
+      },
+    });
+  };
+
+  const onUseDefaultPortChange = (event: ChangeEvent<HTMLInputElement>) => {
+    console.log("onUseDefaultPortChange", event.target.value)
+    onOptionsChange({
+      ...options,
+      jsonData: {
+        ...jsonData,
+        port: 8443,
+        useDefaultPort: !jsonData.useDefaultPort
       },
     });
   };
 
   // Secure field (only sent to the backend)
-  const onAPIKeyChange = (event: ChangeEvent<HTMLInputElement>) => {
+  const onPasswordChange = (event: ChangeEvent<HTMLInputElement>) => {
     onOptionsChange({
       ...options,
       secureJsonData: {
-        apiKey: event.target.value,
+        password: event.target.value,
       },
     });
   };
 
-  const onResetAPIKey = () => {
+  const onResetPassword = () => {
     onOptionsChange({
       ...options,
       secureJsonFields: {
         ...options.secureJsonFields,
-        apiKey: false,
+        password: false,
       },
       secureJsonData: {
         ...options.secureJsonData,
-        apiKey: '',
+        password: '',
       },
     });
   };
 
+  const labelWidth = 12;
+  let defaultInputWidth = 47;
   return (
     <>
-      <InlineField label="Path" labelWidth={14} interactive tooltip={'Json field returned to frontend'}>
+      <Legend>Server</Legend>
+      <InlineField label="Host" labelWidth={labelWidth} interactive tooltip={'Server hostname'}>
         <Input
-          id="config-editor-path"
-          onChange={onPathChange}
-          value={jsonData.path}
-          placeholder="Enter the path, e.g. /api/v1"
-          width={40}
+            id="config-editor-host"
+            onChange={onHostChange}
+            value={jsonData.hostname}
+            placeholder="https://my.hydrolix.domain.com"
+            width={defaultInputWidth}
         />
       </InlineField>
-      <InlineField label="API Key" labelWidth={14} interactive tooltip={'Secure json field (backend only)'}>
+      <InlineField label="Port" labelWidth={labelWidth} interactive tooltip={'Server port number (default: 8443)'}>
+
+        <Stack direction="row">
+          <Input
+            type="number"
+            id="config-editor-port"
+            min={1}
+            max={65535}
+            onChange={onPortChange}
+            value={jsonData.port}
+            placeholder="9440"
+            disabled={jsonData.useDefaultPort}
+            width={29}
+          />
+          <InlineSwitch
+              label="Use default"
+              showLabel={true}
+              onChange={onUseDefaultPortChange}
+              value={jsonData.useDefaultPort}/>
+        </Stack>
+      </InlineField>
+      <Legend>Credentials</Legend>
+      <InlineField label="Username" labelWidth={labelWidth} interactive tooltip={'username to access the system'}>
+        <Input
+          id="config-editor-username"
+          onChange={onUserNameChange}
+          value={jsonData.username}
+          placeholder="username"
+          width={defaultInputWidth}
+        />
+      </InlineField>
+
+      <InlineField label="Password" labelWidth={labelWidth} interactive tooltip={'password to access the system'}>
         <SecretInput
           required
-          id="config-editor-api-key"
-          isConfigured={secureJsonFields.apiKey}
-          value={secureJsonData?.apiKey}
+          id="config-editor-password"
+          isConfigured={secureJsonFields.password}
+          value={secureJsonData?.password}
           placeholder="Enter your API key"
-          width={40}
-          onReset={onResetAPIKey}
-          onChange={onAPIKeyChange}
+          width={defaultInputWidth}
+          onReset={onResetPassword}
+          onChange={onPasswordChange}
         />
       </InlineField>
     </>
