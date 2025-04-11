bb7c4039ffcdc3e04262b4b5831f14ed9072b84f
Volodymyr Kohut <vkohut@hydrolix.io>
2025-04-11 13:55:26 +0100

    Frontend round

--- src/editor/timeRangeUtils.ts
+++ src/editor/timeRangeUtils.ts
@@ -1,7 +1,34 @@
-export const QUERY_DURATION_REGEX = /^$|^0$|^(\d+)(ms|s|m|h|d|w|M|y)$/;
+import {
+  DateTime,
+  dateTime,
+  durationToMilliseconds,
+  parseDuration,
+  TimeRange,
+} from "@grafana/data";
+
+export const QUERY_DURATION_REGEX = /^$|^0$|^(\d+)([smh])$/;
 
 export const getFirstValidRound = (rounds: string[]): string =>
   rounds.find(
     (round) =>
       round !== undefined && round !== "" && QUERY_DURATION_REGEX.test(round)
   ) || "";
+
+export const roundTimeRange = (
+  timeRange: TimeRange,
+  round: string
+): TimeRange => {
+  let duration = durationToMilliseconds(parseDuration(round)) / 1000;
+  if (!duration) {
+    return timeRange;
+  } else {
+    return {
+      from: roundDateTime(timeRange.from, duration),
+      to: roundDateTime(timeRange.to, duration),
+      raw: timeRange.raw,
+    };
+  }
+};
+
+const roundDateTime = (time: DateTime, duration: number) =>
+  dateTime((time.unix() - (time.unix() % duration)) * 1000);
