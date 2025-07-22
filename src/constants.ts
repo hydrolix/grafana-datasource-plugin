7c405d07ad4892f84a29166a2d88e515d1405a24
Volodymyr Kohut <vkohut@hydrolix.io>
2025-07-22 10:28:44 +0100

    GRAP-78 Plugin should be able to limit values for ad-hoc filter.

--- src/constants.ts
+++ src/constants.ts
@@ -16,7 +16,7 @@ export const FUNCTIONS_SQL = "SELECT name FROM  system.functions";
 
 export const AD_HOC_KEY_QUERY = "DESCRIBE ${table}";
 export const AD_HOC_VALUE_QUERY =
-  "SELECT ${column}, COUNT(${column}) as count  FROM ${table} WHERE $__timeFilter(${timeColumn}) AND $__adHocFilter() GROUP BY ${column} ORDER BY count DESC LIMIT 100";
+  "SELECT ${column}, COUNT(${column}) as count  FROM ${table} WHERE $__timeFilter(${timeColumn}) AND $__adHocFilter() ${condition} GROUP BY ${column} ORDER BY count DESC LIMIT 100";
 
 export const SUPPORTED_TYPES = [
   "DateTime",
